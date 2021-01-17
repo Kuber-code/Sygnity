fd27c71bd923b2dca06ce2b07babe025d5051856
Kuber-code <kuba.lichosik@gmail.com>
2021-01-17 23:30:11 +0100

    finished

--- sygnity.cpp
+++ sygnity.cpp
@@ -15,26 +15,51 @@ const int monthDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 int countLeapYears(Date d){
     int years = d.y;
  
-    // Check if the current year needs to be
-    //  considered for the count of leap years
-    // or not
+    // Check if the current year needs to be considered for the count of leap years or not
     if (d.m <= 2)
         years--;
  
-    // An year is a leap year if it 
-    // is a multiple of 4,
-    // multiple of 400 and not a 
-     // multiple of 100.
-    return years / 4 
-           - years / 100
-           + years / 400;
+    // An year is a leap year if it is a multiple of 4, multiple of 400 and not a multiple of 100.
+    return years / 4 - years / 100 + years / 400;
 }
 
-int getDifference(Date dt1, Date dt2)
+float roundFloatToTwoDigits(float var) 
+{ 
+    // EXAMPLE:
+    // 37.66666 * 100 = 3766.66 
+    // 3766.66 + .5 = 3767.16    for rounding off value 
+    // then type cast to int so value is 3767 
+    // then divided by 100 so the value converted into 37.67 
+    float value = (int)(var * 100 + .5); 
+    return (float)value / 100; 
+} 
+
+float getMonthsDifference(Date dt1, Date dt2){
+    // Second Alghoritm:
+    // count months difference between dwo dates
+    long int monthsDifference = (dt2.y * 12 + dt2.m) - (dt1.y * 12 + dt1.m);
+    float completeMonthsFloatDifference= (float) monthsDifference;
+    // count days difference between dwo dates
+    int daysDifference = dt2.d - dt1.d;
+    int daysInLastMonth;
+    // check which was last month (need to know how much of the month is one day for this current month eg.: for February is 1/28, for December is 1/31)
+    if (dt2.d = 1){
+        if(dt2.m = 1){
+            daysInLastMonth = monthDays[12];
+        }
+        daysInLastMonth = monthDays[dt2.m-1];
+    }
+    daysInLastMonth = monthDays[dt2.m];
+    
+    float lastMonthRestDifference =  (float)daysDifference/(float)daysInLastMonth;
+    float totalMonthsDifference = completeMonthsFloatDifference + lastMonthRestDifference;
+    return roundFloatToTwoDigits(totalMonthsDifference);
+}
+
+int getDaysDifference(Date dt1, Date dt2)
 {
-    // COUNT TOTAL NUMBER OF DAYS
-    // BEFORE FIRST DATE 'dt1'
- 
+    // First Alghoritm:
+    // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1'
     // initialize count using years and day
     long int n1 = dt1.y * 365 + dt1.d;
  
@@ -42,13 +67,10 @@ int getDifference(Date dt1, Date dt2)
     for (int i = 0; i < dt1.m - 1; i++)
         n1 += monthDays[i];
  
-    // Since every leap year is of 366 days,
-    // Add a day for every leap year
+    // Since every leap year is of 366 days, Add a day for every leap year
     n1 += countLeapYears(dt1);
  
-    // SIMILARLY, COUNT TOTAL NUMBER OF
-    // DAYS BEFORE 'dt2'
- 
+    // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt2'
     long int n2 = dt2.y * 365 + dt2.d;
     for (int i = 0; i < dt2.m - 1; i++)
         n2 += monthDays[i];
@@ -58,11 +80,10 @@ int getDifference(Date dt1, Date dt2)
     return (n2 - n1);
 }
 
-bool getFileContent(string fileName, vector<string> & vecOfStrings)
+bool getFileContentToVector(string fileName, vector<string> & vecOfStrings)
 {
     // Open the File
     ifstream inputFile(fileName.c_str());
-
     // Check if object is valid
     if(!inputFile)
     {
@@ -70,55 +91,37 @@ bool getFileContent(string fileName, vector<string> & vecOfStrings)
         return false;
     }
     string str;
-    string data[20];
     // Read the next line from File untill it reaches the end.
-    int i = 1;
+    
     while (getline(inputFile, str))
     {
         // Line contains string of length > 0 then save it in vector
         if(str.size() > 0){
             vecOfStrings.push_back(str);
-            //stringstream stream(str);
-            //loop to split string
-            // while(getline(stream, data[i], '-') )
-            // {
-            //     //increase repetition number
-            //     i++;
-            // }
         }
-           
     }
     //Close The File
     inputFile.close();
     return true;
 }
 
-int convertDaysToMonths (int dayDifference){
-    
+float convertDaysToMonths (int dayDifference){
+    // First Alghoritm:
+    float months = (float) dayDifference / 30.42;
+    float months2Decimal = 0.0;
+    months2Decimal = roundFloatToTwoDigits(months);
+    return months2Decimal;
 }
 
-int main()
-{
-    vector<string> vecOfStrings;
-    // Get the contents of file in a vector
-    bool result = getFileContent("input.txt", vecOfStrings);
-    Date dt1 = { 2020, 1, 1 };
-    Date dt2 = { 2020, 1, 2 };
-    if(result){
-        // Print the vector contents
-        vector<string> dataVector;
-        int i=0;
+void convertVectorToStruct(vector<string> & vecOfStrings, Date & dt1, Date & dt2 ){
+        // Split vectors with file content (eg.: 2020-01-01) using String Streams to adequate years, months and days in Data structure
+        int i = 0;
         for(string & line : vecOfStrings){
-            cout << line << endl;
-            // ROBIE COŚ DLA KAŻDEJ LINIK Z TEXTU
-            cout << "linie wektora"<<endl;
-            istringstream ss(line);
+            istringstream stringStream(line);
 	        string token;
-
-            //vector<string> playerInfoVector;
-            while(getline(ss, token, '-')) {
-                dataVector.push_back(token);
-                cout << token << '\n';
+            // Split lines form file to structure data, every token is single data eg.: 2020
+            while(getline(stringStream, token, '-')) {
+                //dataVector.push_back(token);
                 if (i == 0){
                 string s = token;
                 istringstream(s) >> dt1.y ;
@@ -143,19 +146,41 @@ int main()
                 string s = token;
                 istringstream(s) >> dt2.d;
                 }
-                // tutaj wyświetla mi podziuelonego stringa juz
                 i++;
 	        }
         }
-    }
+}
 
-    //Date dt1 = { 2020, 12, 1 };
-    //Date dt2 = { 2020, 12, 2 };
- 
-    // Function call
-    int dayDifference =  getDifference(dt1, dt2);
-    cout << "Difference between two dates is " << dayDifference;
-    int monthDifference = convertDaysToMonths(dayDifference);
+void saveContentToFile(string fileName, float monthDifference){
+    // Create and open a text file
+    ofstream MyFile(fileName);
+    // Write to the file
+    MyFile << monthDifference;
+    // Close the file
+    MyFile.close();
+}
+
+int main()
+{
+    // Declare data
+    vector<string> vecOfStrings;
+    Date dt1 = { 2020, 1, 1 };
+    Date dt2 = { 2020, 1, 2 };
+    // Get the contents of file to vector, if the data is valid
+    bool result = getFileContentToVector("input.txt", vecOfStrings);
+    if(result){
+        convertVectorToStruct(vecOfStrings, dt1, dt2);
+    }
+    // Alghorith 1: count total days difference and count mounths difference from average amount of days per month in the year (365/12)
+    int dayDifference = getDaysDifference(dt1, dt2);
+    cout << "Difference between two dates is " << dayDifference << endl;
+    float monthDifference = convertDaysToMonths(dayDifference);
+    saveContentToFile("output2.txt", monthDifference);
+
+    // Alghoritm 2: count total months difference and add incomplete months (rest of days) as percent of current month eg. 0.07 are difference of 2 days in December
+    float monthDifferenceSecondAlghoritm = getMonthsDifference(dt1, dt2) ;
+    saveContentToFile("output.txt", monthDifferenceSecondAlghoritm);   
+
     system("pause");
     return 0;
 }
